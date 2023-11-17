60ec68f552809165bb9312bd32bdd1629046d414
jeet kanodia <jeetkanodia@gmail.com>
2023-11-18 03:31:37 +0530

    Update CORS origin to localhost:5173

--- server.js
+++ server.js
@@ -8,14 +8,15 @@ const cors = require("cors");
 const app = express();
 
 // middleware
-app.use(express.json());
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:5173",
     methods: "GET, POST, PUT, DELETE",
     allowedHeaders: "Content-Type, Authorization",
   })
 );
+
+app.use(express.json());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
