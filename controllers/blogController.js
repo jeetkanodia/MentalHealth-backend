1981541e0c838a46fbfb23696ec88f6af457e1ee
jeet kanodia <jeetkanodia@gmail.com>
2023-11-19 15:10:47 +0530

    add: blog api

---
+++ controllers/blogController.js
@@ -0,0 +1,100 @@
+const Blog = require("../models/blogModel");
+const mongoose = require("mongoose");
+
+// get all blogs
+const getBlogs = async (req, res) => {
+  const blogs = await Blog.find({}).sort({ createdAt: -1 });
+
+  res.status(200).json(blogs);
+};
+
+// get a single blog
+const getBlog = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such blog" });
+  }
+
+  const blog = await Blog.findById(id);
+
+  if (!blog) {
+    return res.status(404).json({ error: "No such workout" });
+  }
+
+  res.status(200).json(blog);
+};
+
+// create new blog
+const createBlog = async (req, res) => {
+  const { title, description } = req.body;
+
+  let emptyFields = [];
+
+  if (!title) {
+    emptyFields.push("title");
+  }
+  if (!description) {
+    emptyFields.push("description");
+  }
+  if (emptyFields.length > 0) {
+    return res
+      .status(400)
+      .json({ error: "Please fill in all the fields", emptyFields });
+  }
+
+  // add doc to db
+  try {
+    const blog = await Blog.create({ title, description });
+    res.status(200).json(blog);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// delete a blog
+const deleteBlog = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such blog" });
+  }
+
+  const blog = await Blog.findOneAndDelete({ _id: id });
+
+  if (!blog) {
+    return res.status(400).json({ error: "No such blog" });
+  }
+
+  res.status(200).json(blog);
+};
+
+// update a blog
+const updateBlog = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such blog" });
+  }
+
+  const blog = await Blog.findOneAndUpdate(
+    { _id: id },
+    {
+      ...req.body,
+    }
+  );
+
+  if (!blog) {
+    return res.status(400).json({ error: "No such blog" });
+  }
+
+  res.status(200).json(blog);
+};
+
+module.exports = {
+  getBlogs,
+  getBlog,
+  createBlog,
+  deleteBlog,
+  updateBlog,
+};
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const workoutRoutes = require("./routes/workouts");
+const blogRoutes = require("./routes/blogs");
 const userRoutes = require("./routes/user");
 const cors = require("cors");
 // express app
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 });
 
 // routes
-app.use("/api/workouts", workoutRoutes);
+app.use("/api/blogs", blogRoutes);
 app.use("/api/user", userRoutes);
 
 app.get("/", (req, res) => {
